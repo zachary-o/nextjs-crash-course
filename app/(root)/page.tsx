9f52acea203d8589db1eb789296a0700cf87c903
Zakhar Osetskyi <oset.zak@bmbsoft.com.vn>
2025-01-22 16:43:24 +0700

    search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm"
+
+export default async function Home({
+  searchParams
+}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query
+
   return (
     <>
-      <h1 className="heading">hey</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your Startup, <br />
+          Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 
 const Navbar = async () => {
   const session = await auth()
+  
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
